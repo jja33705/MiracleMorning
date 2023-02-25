7e15668ddbadebd30d6e5c6d3597b2d398b3c307
jangjaehyeon <jja33705@gmail.com>
2023-02-25 19:24:10 +0900

    redux-persist 적용시 발생하던 에러 수정

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,16 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { PersistConfig, persistReducer, persistStore } from 'redux-persist';
+import {
+  PersistConfig,
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import timeReducer from './reducers/timeSlice';
 import toDoReducer from './reducers/toDoSlice';
 
@@ -19,6 +29,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 const persistor = persistStore(store);
 
